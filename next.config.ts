9f25918d42fb1e570563e20faacd7d45fa17a7a9
Andrew Local <andrew.buist99@outlook.com>
2025-08-21 13:27:39 +0100

    init

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'portfolio_js';
+
+module.exports = {
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
