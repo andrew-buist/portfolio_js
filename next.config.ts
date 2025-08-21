caa3afff244d7a8c858564326ba81f8b5c90a640
Andrew Local <andrew.buist99@outlook.com>
2025-08-21 13:36:25 +0100

    changes to reference absolute path

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'portfolio_js';
-
-module.exports = {
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
-  trailingSlash: true
-};
-
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/portfolio_js'
 };
 
 export default nextConfig;
